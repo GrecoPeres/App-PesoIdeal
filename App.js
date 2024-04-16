51f32339be8eaed275796c1e39e1039d3ba4cb00
Greco Fernando M.P <100162697+GrecoPeres@users.noreply.github.com>
2024-04-16 10:54:35 -0300

    Initial Commit - I'm Include navigation bottom navbar

--- App.js
+++ App.js
@@ -1,20 +1,19 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { Home, Dashboard, Historico } from './screens';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+
+const Tab = createBottomTabNavigator()
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Tab.Navigator>
+        <Tab.Screen name="Home" component={Home} />
+        <Tab.Screen name="Dashboard" component={Dashboard} />
+        <Tab.Screen name="Histórico" component={Historico} />
+      </Tab.Navigator>
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ screens/index.js
@@ -0,0 +1,9 @@
+import Home from "./Home";
+import Dashboard from "./Dashboard";
+import Historico from "./Historico";
+
+export {
+    Home,
+    Dashboard,
+    Historico
+}
